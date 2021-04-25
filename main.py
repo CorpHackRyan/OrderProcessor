ba76fd2c74557a327b2e159ee1b61deef0a7905d
Ryan OConnor <r2oconnor@student.bridgew.edu>
2021-04-25 18:51:30 -0400

    added requirements.txt import dataclasses instantiate record dataclass add notes to test_main.py

--- main.py
+++ main.py
@@ -1,17 +1,46 @@
-# This is a sample Python script.
+from dataclasses import dataclass
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# calculate the total to charge a customer at checkout.
+# function should take the users state and a list of records* of items to be purchased,
+# it should return the total that the user will be charged (including sales tax).
+
+# Let’s limit the states we do business in to Massachusetts. New Hampshire and Maine.
+# We will limit the types of goods we sell (in the record type field) to these three for simplicity:
+# “Wic Eligible food”, Clothing, and “everything else”
+
+# data class in python, a record or something similar in java – with data
+# members but no useful methods, put the basic record class in your file so your tests can create test work.
+# The record should have three fields, the item name, the price, and the item type.
+
+# Write automated tests
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@dataclass
+class Record:
+    item_name: str
+    item_price: float
+    item_type: str
+
+def calculate_total_charge(abbr_state, items):
+    state_tax = {'MA': '.0625', 'NH': '0.0', 'VT': ".06" }
+    amount = 0.0
+
+    if abbr_state != 'MA':
+        return('State entered is not valid. Please choose MA, NH or VT.')
+
+    return 'total that user will be charged'
+
+    # if abbr_state
+    # // Code to calculate total user will be charged here
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    print('make a change')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    list_of_food = {}
+
+    #state = 'bad state'
+    state = "MA"
+    #state = 'NH'
+    #state = 'VT'
+
+    print(calculate_total_charge(abbr_state=state, items=list_of_food))
---
+++ tests/test_main.py
@@ -0,0 +1,9 @@
+# Write automated tests to thoroughly test the function your are writing above.
+
+# Upload the production and test code to github and invite your instructor to the github project.
+# Include a readme.md that tells me what your production function is and how to run your tests.
+
+# Test Good Cases
+# Test Bad Cases
+# Test edge cases
+# Research other style of automated stesting
