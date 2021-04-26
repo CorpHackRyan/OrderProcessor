6c7ff22417edf7eb31b7c21e1e55fe73d56dbf91
Ryan OConnor <r2oconnor@student.bridgew.edu>
2021-04-26 17:19:58 -0400

    added more tests

--- main.py
+++ main.py
@@ -3,21 +3,6 @@ from dataclasses import dataclass
 # 4/25/21 - 45 minutes invested
 
 
-# calculate the total to charge a customer at checkout.
-# function should take the users state and a list of records* of items to be purchased,
-# it should return the total that the user will be charged (including sales tax).
-
-# Let’s limit the states we do business in to Massachusetts. New Hampshire and Maine.
-# We will limit the types of goods we sell (in the record type field) to these three for simplicity:
-# “Wic Eligible food”, Clothing, and “everything else”
-
-# data class in python, a record or something similar in java – with data
-# members but no useful methods, put the basic record class in your file so your tests can create test work.
-# The record should have three fields, the item name, the price, and the item type.
-
-# Write automated tests
-
-
 @dataclass
 class Record:
     item_name: str
@@ -26,12 +11,27 @@ class Record:
 
 
 def calculate_total_charge(abbr_state, list_of_records):
+    # Calculate the total to charge a customer at checkout.
+    # Function should take the users state and a list of records* of items to be purchased,
     state_tax = {'MA': '0.0625',
                  'NH': '0.0',
-                 'VT': "0.06" }
+                 'VT': "0.06"}
 
-    print(abbr_state, list_of_records.item_type)
+    #print(list_of_records)
+    print('\n')
+    
+    for each_record in list_of_records:
+
+        print(each_record.item_name, each_record.item_price, each_record.item_type)
+
+
+
+    # Types of goods accepted: Wic Eligible food, Clothing, everything else
+    #print(abbr_state, list_of_records[2].item_price)
+    #print(len(list_of_records))
+
     # return 'total that user will be charged'
+    # It should return the total that the user will be charged (including sales tax).
 
     # if abbr_state
     # // Code to calculate total user will be charged here
@@ -40,6 +40,6 @@ def calculate_total_charge(abbr_state, list_of_records):
 
 if __name__ == '__main__':
 
-    calculate_total_charge('MA', new_item)
-
-    #print(calculate_total_charge(abbr_state=state, items=list_of_food))
+    # calculate_total_charge('MA', new_item)
+    # print(calculate_total_charge(abbr_state=state, items=list_of_food))
+    pass
--- tests/test_main.py
+++ tests/test_main.py
@@ -12,5 +12,13 @@ import main
 
 
 def test_calculate_total_charge():
-    new_item = main.Record('shirt', 1.50, 'Clothing')
-    main.calculate_total_charge('MA', new_item)
+    new_record1 = main.Record('shirt', 1.50, 'Clothing')
+    new_record2 = main.Record('baby formula', 3.00, 'Wic Eligible food')
+    new_record3 = main.Record('TV', 6.00, 'everything else')
+    all_records = [new_record3, new_record2, new_record1]
+
+    for all_recs in all_records:
+        #print(all_recs)
+        pass
+
+    main.calculate_total_charge('MA', all_records)
