cd4204d0cb094dc7f73924f3b6da3aa26e77dbda
Ryan OConnor <r2oconnor@student.bridgew.edu>
2021-04-26 19:45:37 -0400

    updated functions in tests & order_processor

--- order_processor.py
+++ order_processor.py
@@ -1,7 +1,11 @@
+# Calculate the total to charge a customer at checkout based on which state its purchased from and if the item is
+# eligible to be taxed.
+
 from dataclasses import dataclass
-
-
+# 4/24/21 - 10 mins
 # 4/25/21 - 45 minutes invested
+# 4/26/21 - 1st sesh: 1 hr 15 mins,
+#           2nd sesh: 25 mions
 
 
 @dataclass
@@ -12,19 +16,36 @@ class Record:
 
 
 def calculate_total_charge(abbr_state, list_of_records):
-    # Calculate the total to charge a customer at checkout.
+
     # Function should take the users state and a list of records* of items to be purchased,
     state_tax = {'MA': '0.0625',
-                 'NH': '0.0',
                  'VT': "0.06"}
-
-    # print(list_of_records)
+    total_cost = 0.0
     print('\n')
 
-    for each_record in list_of_records:
-        print(each_record.item_name, each_record.item_price, each_record.item_type)
-
     # Types of goods accepted: Wic Eligible food, Clothing, everything else
+    # 'Clothing' is tax exempt in NH and VT. Clothing is taxed in MA after 175$.
+    # '(WIC) Grocery' is exempt from all states
+    # 'Everything else' is taxed by VT and MA and NOT NH.
+
+    # NH = tax exempt from: ALL
+    # VT = tax exempt from: Clothing, WIC
+    # MA = tax exempt from: Clothing(past $175), WIC
+
+    for each_record in list_of_records:
+        if abbr_state == "NH":
+            total_cost = total_cost + each_record.item_price
+
+        elif each_record.item_type == "Clothing":
+            total_cost = total_cost + (each_record.item_price * state_tax[abbr_state])
+
+        elif each_record.item_type == "VT":
+            pass
+            # print(each_record.item_name, each_record.item_price, each_record.item_type)
+        total_cost = total_cost + each_record.item_price
+
+    print('Total cost is: $', total_cost)
+
     # print(abbr_state, list_of_records[2].item_price)
     # print(len(list_of_records))
 
--- tests/test_order_processor.py
+++ tests/test_order_processor.py
@@ -10,15 +10,23 @@
 
 import order_processor
 
+MA = "MA"
+VT = "VT"
+NH = "NH"
+
 
 def test_calculate_total_charge():
-    new_record1 = order_processor.Record('shirt', 1.50, 'Clothing')
+
+
+    new_record1 = order_processor.Record('shirt', 1.55, 'Clothing')
     new_record2 = order_processor.Record('baby formula', 3.00, 'Wic Eligible food')
     new_record3 = order_processor.Record('TV', 6.00, 'everything else')
+
     all_records = [new_record3, new_record2, new_record1]
 
     for all_recs in all_records:
+
         #print(all_recs)
         pass
 
-    order_processor.calculate_total_charge('MA', all_records)
+    order_processor.calculate_total_charge(MA, all_records)
