d51453e32654696a98718155b09fe07e54570b81
Ryan OConnor <r2oconnor@student.bridgew.edu>
2021-04-26 21:05:02 -0400

    function complete - added test case

--- order_processor.py
+++ order_processor.py
@@ -1,13 +1,15 @@
 # Calculate the total to charge a customer at checkout based on which state its purchased from and if the item is
 # eligible to be taxed.
-
-from dataclasses import dataclass
 # 4/24/21 - 10 mins
 # 4/25/21 - 45 minutes invested
 # 4/26/21 - 1st sesh: 1 hr 15 mins,
 #           2nd sesh: 25 mions
 #           3rd sesh: 25 mins
-#           4th sesh:
+#           4th sesh: 25 mins
+#           5th sesh:
+
+from dataclasses import dataclass
+
 
 @dataclass
 class Record:
@@ -30,7 +32,14 @@ def calculate_total_charge(abbr_state, list_of_records):
     for each_record in list_of_records:
         if abbr_state == "NH":
             total_cost = total_cost + each_record.item_price
-            print('NH')  # works fine
+
+        elif abbr_state == "VT":
+            if each_record.item_type == "Wic Eligible food":
+                total_cost = total_cost + each_record.item_price
+            elif each_record.item_type == "Clothing":
+                total_cost = total_cost + each_record.item_price
+            elif each_record.item_type == "everything else":
+                total_cost = total_cost + (each_record.item_price * state_tax[abbr_state]) + each_record.item_price
 
         elif abbr_state == "MA":
             if each_record.item_type == "Wic Eligible food":
@@ -42,29 +51,15 @@ def calculate_total_charge(abbr_state, list_of_records):
                     total_cost = total_cost + tax_total + each_record.item_price
                 else:
                     total_cost = total_cost + each_record.item_price
-
-
             elif each_record.item_type == "everything else":
-                total_cost = total_cost + (total_cost * state_tax[abbr_state])
+                total_cost = total_cost + (each_record.item_price * state_tax[abbr_state]) + each_record.item_price
 
-        elif abbr_state == "VT":
-            print("VT was submitted")
         else:
-            print(abbr_state, "state is not valid")
-            return "State is not valid."
+            print(abbr_state, "State is not valid")
+            return -1
 
-            # print(each_record.item_name, each_record.item_price, each_record.item_type)
-
-        # elif (state doesn't exist, return an error message state isn't valid)
-
-
-    print('Total cost is: $', round(total_cost, 2))
-
-    # print(len(list_of_records))
-    # It should return the total that the user will be charged (including sales tax).
+    return total_cost
 
 
 if __name__ == '__main__':
     pass
-    # calculate_total_charge('MA', new_item)
-    # print(calculate_total_charge(abbr_state=state, items=list_of_food))
--- tests/test_order_processor.py
+++ tests/test_order_processor.py
@@ -1,12 +1,6 @@
-# Write automated tests to thoroughly test the function your are writing above.
-
-# Upload the production and test code to github and invite your instructor to the github project.
+# Invite your instructor to the github project.
 # Include a readme.md that tells me what your production function is and how to run your tests.
-
-# Test Good Cases
-# Test Bad Cases
-# Test edge cases
-# Research other style of automated stesting
+# Test Good Cases / Test Bad Cases / Test edge cases / Research other style of automated testing
 
 import order_processor
 
@@ -16,17 +10,15 @@ NH = "NH"
 
 
 def test_calculate_total_charge():
-
-
-    new_record1 = order_processor.Record('shirt', 275, 'Clothing')
-    new_record2 = order_processor.Record('baby formula', 0, 'Wic Eligible food')
-    new_record3 = order_processor.Record('TV', 0, 'everything else')
+    new_record1 = order_processor.Record('shirt', 100, 'Clothing')
+    new_record2 = order_processor.Record('baby formula', 100, 'Wic Eligible food')
+    new_record3 = order_processor.Record('TV', 300, 'everything else')
 
     all_records = [new_record3, new_record2, new_record1]
 
     for all_recs in all_records:
-
-        #print(all_recs)
+        # print(all_recs)
         pass
 
-    order_processor.calculate_total_charge(MA, all_records)
+    assert (order_processor.calculate_total_charge("MAS", all_records) != -1)
+
